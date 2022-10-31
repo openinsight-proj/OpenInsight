fbf0ea2bfa21cd2bccede9c4c39850bf2a254e27
minquan.chen <minquan.chen@daocloud.io>
2022-10-31 21:05:41 +0800

    ut

--- extension/query/query_server_test.go
+++ extension/query/query_server_test.go
@@ -5,9 +5,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
-	"go.opentelemetry.io/collector/config/configgrpc"
-	"go.opentelemetry.io/collector/config/confighttp"
-	"go.opentelemetry.io/collector/config/confignet"
 	"go.opentelemetry.io/collector/service/servicetest"
 	"path/filepath"
 	"testing"
@@ -18,26 +15,14 @@ func Test_query_server(t *testing.T) {
 	require.NoError(t, err)
 
 	factory := NewFactory()
-
 	factories.Extensions[typeStr] = factory
 	cfg, err := servicetest.LoadConfigAndValidate(filepath.Join("testdata", "test-config.yaml"), factories)
 	require.NoError(t, err)
 	require.NotNil(t, cfg)
 
-	cfg_e := cfg.Extensions[config.NewComponentID(typeStr)]
-
-	cfg_e.(*Config).Protocols.Grpc = &configgrpc.GRPCServerSettings{
-		NetAddr: confignet.NetAddr{
-			Endpoint:  defaultGRPCBindEndpoint,
-			Transport: "tcp",
-		},
-	}
-
-	cfg_e.(*Config).Protocols.Http = &confighttp.HTTPServerSettings{
-		Endpoint: defaultHTTPBindEndpoint,
-	}
-
-	ext, err := createExtension(context.Background(), componenttest.NewNopExtensionCreateSettings(), cfg_e)
+	ext, err := createExtension(context.Background(),
+		componenttest.NewNopExtensionCreateSettings(),
+		cfg.Extensions[config.NewComponentID(typeStr)])
 	require.NoError(t, err)
 	require.NotNil(t, ext)
 	err = ext.Start(context.Background(), nil)
@@ -45,5 +30,4 @@ func Test_query_server(t *testing.T) {
 		return
 	}
 	select {}
-
 }
