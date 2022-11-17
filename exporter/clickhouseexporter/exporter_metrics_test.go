c58d26ac4b69b98e88018f0fb2a5ef12a9701a65
Jared Tan <jian.tan@daocloud.io>
2022-11-17 10:16:44 +0800

    update ci
    Signed-off-by: Jared Tan <jian.tan@daocloud.io>

--- exporter/clickhouseexporter/exporter_metrics_test.go
+++ exporter/clickhouseexporter/exporter_metrics_test.go
@@ -3,12 +3,10 @@ package clickhouseexporter
 import (
 	"context"
 	"database/sql/driver"
-	"fmt"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.uber.org/zap/zaptest"
-	"log"
 	"strings"
 	"testing"
 	"time"
@@ -105,7 +103,7 @@ func newTestMetricsExporter(t *testing.T, dsn string, fns ...func(*Config)) *met
 }
 
 func TestExporter_pushMetricsData(t *testing.T) {
-	t.Run("push sucess", func(t *testing.T) {
+	t.Run("push success", func(t *testing.T) {
 		var items int
 		initClickhouseTestServer(t, func(query string, values []driver.Value) error {
 			t.Logf("%d, values:%+v", items, values)
@@ -128,88 +126,88 @@ func Test_newMetricsExporter(t *testing.T) {
 }
 
 // still in process
-func Test_tran(t *testing.T) {
-	exporter := newTestMetricsExporter(t, defaultDSN)
-	db := exporter.client
-	tx, err := db.Begin()
-	if err != nil {
-		fmt.Errorf("db.Begin: %w", err)
-	}
-
-	createSQL1 := `
-CREATE TABLE IF NOT EXISTS foo (
-    IntValue Int64,
-    Exemplars Nested (
-		Attributes Map(LowCardinality(String), String)
-    ) CODEC(ZSTD(1))          
-) ENGINE MergeTree()
-PARTITION BY IntValue
-ORDER BY IntValue
-SETTINGS index_granularity=8192, ttl_only_drop_parts = 1;`
-
-	insertSQL1 := "INSERT INTO foo (IntValue) VALUES (?)"
-
-	createSQL2 := `
-CREATE TABLE IF NOT EXISTS bar (
-    IntValue Int64,
-    Exemplars Nested (
-		Attributes Map(LowCardinality(String), String)
-    ) CODEC(ZSTD(1))          
-) ENGINE MergeTree()
-PARTITION BY IntValue
-ORDER BY IntValue
-SETTINGS index_granularity=8192, ttl_only_drop_parts = 1;`
-
-	insertSQL2 := "INSERT INTO bar (IntValue) VALUES (?)"
-
-	valueArgs := []interface{}{
-		int64(14),
-	}
-
-	_, err = db.Exec(createSQL1)
-	require.NoError(t, err)
-	_, err = db.Exec(createSQL2)
-	require.NoError(t, err)
-
-	ctx := context.Background()
-	tx, err = db.BeginTx(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//use Prepare error:
-	//table 1
-	//dbStmt, err := db.Prepare(insertSQL1)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//_, err = tx.Stmt(dbStmt).Exec(valueArgs...)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//table 2
-	//dbStmt, err = db.Prepare(insertSQL2)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//_, err = tx.Stmt(dbStmt).Exec(valueArgs...)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	_, err = tx.ExecContext(ctx, insertSQL1, valueArgs...)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = tx.ExecContext(ctx, insertSQL2, valueArgs...)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = tx.Rollback()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
+//func Test_tran(t *testing.T) {
+//	exporter := newTestMetricsExporter(t, defaultDSN)
+//	db := exporter.client
+//	tx, err := db.Begin()
+//	if err != nil {
+//		fmt.Printf("db.Begin: %w", err)
+//	}
+//
+//	createSQL1 := `
+//CREATE TABLE IF NOT EXISTS foo (
+//    IntValue Int64,
+//    Exemplars Nested (
+//		Attributes Map(LowCardinality(String), String)
+//    ) CODEC(ZSTD(1))
+//) ENGINE MergeTree()
+//PARTITION BY IntValue
+//ORDER BY IntValue
+//SETTINGS index_granularity=8192, ttl_only_drop_parts = 1;`
+//
+//	insertSQL1 := "INSERT INTO foo (IntValue) VALUES (?)"
+//
+//	createSQL2 := `
+//CREATE TABLE IF NOT EXISTS bar (
+//    IntValue Int64,
+//    Exemplars Nested (
+//		Attributes Map(LowCardinality(String), String)
+//    ) CODEC(ZSTD(1))
+//) ENGINE MergeTree()
+//PARTITION BY IntValue
+//ORDER BY IntValue
+//SETTINGS index_granularity=8192, ttl_only_drop_parts = 1;`
+//
+//	insertSQL2 := "INSERT INTO bar (IntValue) VALUES (?)"
+//
+//	valueArgs := []interface{}{
+//		int64(14),
+//	}
+//
+//	_, err = db.Exec(createSQL1)
+//	require.NoError(t, err)
+//	_, err = db.Exec(createSQL2)
+//	require.NoError(t, err)
+//
+//	ctx := context.Background()
+//	tx, err = db.BeginTx(ctx, nil)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	//use Prepare error:
+//	//table 1
+//	//dbStmt, err := db.Prepare(insertSQL1)
+//	//if err != nil {
+//	//	log.Fatal(err)
+//	//}
+//	//_, err = tx.Stmt(dbStmt).Exec(valueArgs...)
+//	//if err != nil {
+//	//	log.Fatal(err)
+//	//}
+//
+//	//table 2
+//	//dbStmt, err = db.Prepare(insertSQL2)
+//	//if err != nil {
+//	//	log.Fatal(err)
+//	//}
+//	//_, err = tx.Stmt(dbStmt).Exec(valueArgs...)
+//	//if err != nil {
+//	//	log.Fatal(err)
+//	//}
+//
+//	_, err = tx.ExecContext(ctx, insertSQL1, valueArgs...)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	_, err = tx.ExecContext(ctx, insertSQL2, valueArgs...)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	err = tx.Rollback()
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//}
